dabd1b4bcb455f402172d7ea40e50623c7eb72a2
fabbernat <fabbernat@gmail.com>
2025-10-10 11:02:10 +0200

    add Camp model

---
+++ MyMvvmApp/Models/Camp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Camp
+{
+	private int _id;
+	private static int _nextId = 1;
+
+	public Date startDate = Date.Today;
+	public Date endDate = Date.Today + 7 days;
+    public Camp()
+	{
+		_id = _nextId++;
+    }
+
+	public Camp(Date startDate, Date endDate)
+	{
+		_id = _nextId++;
+		this.startDate = startDate;
+		this.endDate = endDate;
+    }
+}
