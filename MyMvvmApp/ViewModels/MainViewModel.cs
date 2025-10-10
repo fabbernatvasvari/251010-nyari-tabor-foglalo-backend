0c6b0e2730e018ee99027ff814bb6c3ea2703dae
fabbernat <fabbernat@gmail.com>
2025-10-10 09:44:45 +0200

    todo fix consoleapp because  it runs  to errors rn

---
+++ MyMvvmApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace MyMvvmApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _message = ""Hello, MVVM!"";
+
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                if (_message != value)
+                {
+                    _message = value;
+                    OnPropertyChanged(nameof(Message));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
